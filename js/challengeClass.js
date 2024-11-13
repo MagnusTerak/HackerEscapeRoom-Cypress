0b55524a04e831670cd12fb37a3afa4c191451ea
El-Duckster <144563447+El-Duckster@users.noreply.github.com>
2024-11-13 12:53:18 +0100

    Add constructor to Challenge class

---
+++ js/challengeClass.js
@@ -0,0 +1,14 @@
+class Challenge{
+    constructor(id,title, description, type, minParticipants, maxParticipants, rating, image,labels ){
+        this.id=id;
+        this.title= title;
+        this.description= description;
+        this.type=type;
+        this.minParticipants= minParticipants;
+        this.maxParticipants=maxParticipants;
+        this.rating=rating;
+        this.image= image;
+        this.labels=labels;
+
+    } 
+}
