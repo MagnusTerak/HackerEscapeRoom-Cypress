feebc1ea8450b2ade0aee16d79bb12310f0a1730
Kirill <kirillsavorin@gmail.com>
2024-11-13 20:09:54 +0100

    Class names changed to origins and some small fixes applied

--- js/challengeClass.js
+++ js/challengeClass.js
@@ -24,12 +24,13 @@ export class Challenge {
   createRatingStars() {
     // Funktion for rating(under construction)
     const challengeRating = document.createElement("div");
-    challengeRating.className = "challenge__ratings"
+    challengeRating.className = "room__ratings"
 
 
     for (let i = 0; i < 5; i++) {
       const star = new Image();
-      star.src = "assets/svg/star-filled.svg"
+      star.src = "assets/svg/star-filled.svg";
+      star.className = "star filled";
       //star.src = "assets/svg/star.svg"
       challengeRating.appendChild(star);
     }
@@ -40,15 +41,15 @@ export class Challenge {
   //methods:
   createChallengeCard() {
     const liItem = document.createElement("li");
-    liItem.id = "challenge__carousel__slide" + this.id;
-    liItem.tagIndex = 0;
+    liItem.id = "room__carousel__slide" + this.id;
+    liItem.setAttribute('tabindex', 0);
     liItem.className = "carousel__slide roomy room" + this.id;
 
     const imageContainer = document.createElement("div");
-    imageContainer.className = "challenge__image-container"
+    imageContainer.className = "room__image-container"
 
     const img = document.createElement("img");
-    img.className = "challenge_img";
+    img.className = "room__img";
     img.src = this.image;
     img.alt = this.description;
     img.loading = "lazy";
@@ -58,30 +59,31 @@ export class Challenge {
     imageContainer.appendChild(img);
 
     const detailsContainer = document.createElement("div");
-    detailsContainer.className = "challenge_details";
+    detailsContainer.className = "room__details";
+    
 
     const title = document.createElement("h2");
-    title.className = "challenge__details__title";
-    title.textContent = `${this.title} (${this.type})`;
+    title.className = "room__details__title";
+    title.textContent = this.title;
 
     const detailsInfo = document.createElement("div");
-    detailsInfo.className = "challenge__details-info";
+    detailsInfo.className = "room__details-info";
 
     const rating = this.createRatingStars();
 
     const challengeParticipants = document.createElement("div");
-    challengeParticipants.className = "challenge__participants";
-    challengeParticipants.textContent = `${this.minParticipants} - ${this.maxParticipants}`;
+    challengeParticipants.className = "room__participants";
+    challengeParticipants.textContent = `${this.minParticipants} - ${this.maxParticipants} participants`;
 
     detailsInfo.appendChild(rating);
     detailsInfo.appendChild(challengeParticipants);
 
     const challengeDescription = document.createElement("p");
-    challengeDescription.className = "challenge__description"
+    challengeDescription.className = "room__description"
     challengeDescription.textContent = this.description;
 
     const link = document.createElement("a");
-    link.className = "challenge_btn";
+    link.className = "room__btn";
     link.textContent = "Book this challenge";
 
     detailsContainer.appendChild(title);
