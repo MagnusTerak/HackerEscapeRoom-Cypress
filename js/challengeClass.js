93c6b032439d5a34821df927287dc98077c4d15a
El-Duckster <144563447+El-Duckster@users.noreply.github.com>
2024-11-13 12:54:45 +0100

    Update format of Challenge class

--- js/challengeClass.js
+++ js/challengeClass.js
@@ -1,14 +1,23 @@
-class Challenge{
-    constructor(id,title, description, type, minParticipants, maxParticipants, rating, image,labels ){
-        this.id=id;
-        this.title= title;
-        this.description= description;
-        this.type=type;
-        this.minParticipants= minParticipants;
-        this.maxParticipants=maxParticipants;
-        this.rating=rating;
-        this.image= image;
-        this.labels=labels;
-
-    } 
+class Challenge {
+  constructor(
+    id,
+    title,
+    description,
+    type,
+    minParticipants,
+    maxParticipants,
+    rating,
+    image,
+    labels,
+  ) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.type = type;
+    this.minParticipants = minParticipants;
+    this.maxParticipants = maxParticipants;
+    this.rating = rating;
+    this.image = image;
+    this.labels = labels;
+  }
 }
