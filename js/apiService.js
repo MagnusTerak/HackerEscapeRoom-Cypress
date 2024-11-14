fdaba91a737288c9371344471e10d8750b9c75bb
El-Duckster <144563447+El-Duckster@users.noreply.github.com>
2024-11-14 11:42:16 +0100

    create and test function fetchAvailableTimes

--- js/apiService.js
+++ js/apiService.js
@@ -1,14 +1,35 @@
 const BASE_URL = 'https://lernia-sjj-assignments.vercel.app/api';
-console.log('hello from apiTest')
-export const fetchAllChallenges= async()=>{
-    try{
-        const res = await fetch(`${BASE_URL}/challenges`);
-        const {challenges}= await res.json();
-        console.log(challenges);
-        console.log(typeof(challenges));
-        return challenges?? [];
 
-    }catch(error){
-console.error("Error in feching challenges", error);
-    }
-}
+export const fetchAllChallenges = async () => {
+  try {
+    const res = await fetch(`${BASE_URL}/challenges`);
+    const { challenges } = await res.json();
+    console.log('challengs:', challenges);
+    console.log(typeof challenges);
+    return challenges ?? [];
+  } catch (error) {
+    console.error('Error in feching challenges', error);
+    return [];
+  }
+};
+
+export const fetchAvailableTimes = async (challengeId, date) => {
+  try {
+    const res = await fetch(
+      `${BASE_URL}/booking/available-times?date=${date}&challenge=${challengeId}`
+);
+console.log('challengeId:', typeof(challengeId), 'dateType:' , typeof(date))
+
+if (!res.ok) {
+    const errorText = await res.text(); 
+    console.error('Error response:', errorText);
+    throw new Error(`HTTP error! status: ${res.status}`);
+  }
+    const { slots } = await res.json();
+    console.log('available slots:',slots);
+    return slots ?? [];
+  } catch (error) {
+    console.error('Error fetching available times:', error);
+    return [];
+  }
+};
--- js/apiTest.js
+++ js/apiTest.js
@@ -1,2 +1,3 @@
-import { fetchAllChallenges } from "./apiService.js";
+import { fetchAllChallenges, fetchAvailableTimes } from "./apiService.js";
 fetchAllChallenges();
+fetchAvailableTimes(3, '2024-12-12');
