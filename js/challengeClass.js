4a02672c5c69eec818baad63faee3a3cc8edae91
Kirill <kirillsavorin@gmail.com>
2024-11-15 22:17:37 +0100

    new class names for function createChallengeCard js imported to index, temporary

--- js/challengeClass.js
+++ js/challengeClass.js
@@ -21,18 +21,23 @@ export class Challenge {
     this.labels = labels;
   }
 
-  createRatingStars() {
+  createChallengeStars() {
     // Funktion for rating(under construction)
     const challengeRating = document.createElement("div");
     challengeRating.className = "room__ratings"
-
+    const starFilled = new Image();
+    starFilled.src = "assets/svg/star-filled.svg";
+    starFilled.className = "star filled";
+    const starEmpty = new Image();
+    starEmpty.src = "assets/svg/star.svg";
+    starEmpty.className = "star unfilled";
 
     for (let i = 0; i < 5; i++) {
-      const star = new Image();
-      star.src = "assets/svg/star-filled.svg";
-      star.className = "star filled";
       //star.src = "assets/svg/star.svg"
-      challengeRating.appendChild(star);
+      //star.src = "assets/svg/star-half.svg"
+      challengeRating.appendChild(starFilled);
+      challengeRating.appendChild(starEmpty);
+      
     }
 
     return challengeRating;
@@ -43,13 +48,13 @@ export class Challenge {
     const liItem = document.createElement("li");
     liItem.id = "room__carousel__slide" + this.id;
     liItem.setAttribute('tabindex', 0);
-    liItem.className = "carousel__slide roomy room" + this.id;
+    liItem.className = "challenge__slide challengey challenges" + this.id;
 
     const imageContainer = document.createElement("div");
-    imageContainer.className = "room__image-container"
+    imageContainer.className = "challenge__image-container"
 
     const img = document.createElement("img");
-    img.className = "room__img";
+    img.className = "challenge__img";
     img.src = this.image;
     img.alt = this.description;
     img.loading = "lazy";
@@ -59,31 +64,31 @@ export class Challenge {
     imageContainer.appendChild(img);
 
     const detailsContainer = document.createElement("div");
-    detailsContainer.className = "room__details";
+    detailsContainer.className = "challenge__details";
     
 
     const title = document.createElement("h2");
-    title.className = "room__details__title";
+    title.className = "challenge__details__title";
     title.textContent = this.title;
 
     const detailsInfo = document.createElement("div");
-    detailsInfo.className = "room__details-info";
+    detailsInfo.className = "challenge__details-info";
 
-    const rating = this.createRatingStars();
+    const rating = this.createChallengeStars();
 
     const challengeParticipants = document.createElement("div");
-    challengeParticipants.className = "room__participants";
+    challengeParticipants.className = "challenge__participants";
     challengeParticipants.textContent = `${this.minParticipants} - ${this.maxParticipants} participants`;
 
     detailsInfo.appendChild(rating);
     detailsInfo.appendChild(challengeParticipants);
 
     const challengeDescription = document.createElement("p");
-    challengeDescription.className = "room__description"
+    challengeDescription.className = "challenge__description"
     challengeDescription.textContent = this.description;
 
     const link = document.createElement("a");
-    link.className = "room__btn";
+    link.className = "challenge__btn";
     link.textContent = "Book this challenge";
 
     detailsContainer.appendChild(title);
@@ -97,3 +102,19 @@ export class Challenge {
     return liItem;
   }
 }
+
+
+const challenge = new Challenge(
+  1,
+  "Title of room",
+  "Praeterea, ex culpa non invenies unum aut non accusatis unum. Et nihil inuitam. Nemo nocere tibi erit, et non inimicos, et. ",
+  "on-site",
+  2,
+  6,
+  4.5,
+  "assets/images/gallery/image1.png",
+  ""
+  );
+  
+  const roomList = document.querySelector(".challenges__list");
+  roomList.appendChild(challenge.createChallengeCard());
