4130924ca60b28a36dcbe3041bdb14f2e1756fc2
El-Duckster <144563447+El-Duckster@users.noreply.github.com>
2024-11-18 23:38:16 +0100

    install and configure vite

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // You can add global Sass options here if needed
+      },
+    },
+  },
+});
