3d7372d332ced210164d9d08c7c880163fe3cad1
Deansie <dennis.andersen@lerniastudent.se>
2024-11-19 21:01:24 +0100

    Added checkboxes in open filterbox, allowing visitors to include online/on-site and/or exclude online/on-site challenges in their filtering. As of now there are no functions assosciated with the checkboxes.

--- js/filteringBox.js
+++ js/filteringBox.js
@@ -27,6 +27,52 @@ function openFilterModal() {
     closeFilterButton.classList.add("filter__box__topDiv__closeFilterBtn");
     closeFilterButton.innerHTML = "X";
     closeFilterButton.addEventListener("click", closeFilterModal);
+
+    // Checkbox divs
+    const checkDivMain = document.createElement("div");
+    filterBox.appendChild(checkDivMain);
+    checkDivMain.classList.add("filter__box__checkDivMain");
+
+    const checkDiv2 = document.createElement("div");
+    checkDivMain.appendChild(checkDiv2);
+    checkDiv2.classList.add("filter__box__checkDivMain__checkDiv2");
+
+    const checkDiv3 = document.createElement("div");
+    checkDivMain.appendChild(checkDiv3);
+    checkDiv3.classList.add("filter__box__checkDivMain__checkDiv3");
+
+    const checkDiv4 = document.createElement("div");
+    checkDivMain.appendChild(checkDiv4);
+    checkDiv4.classList.add("filter__box__checkDivMain__checkDiv4");
+
+    // Checkboxes
+    const checkBoxTitle = document.createElement("h4");
+    checkBoxTitle.innerHTML = "By type";
+    checkDiv2.appendChild(checkBoxTitle);
+    checkBoxTitle.classList.add("filter__box__checkDiv__title");
+
+    const checkBox1 = document.createElement("input");
+    checkBox1.type = ("checkbox");
+    checkBox1.id = "DOM__checkBox1";
+    checkDiv3.appendChild(checkBox1);
+    checkBox1.classList.add("filter__box__checkDiv__checkbox1");
+
+    const checkBoxLabel1 = document.createElement("label");
+    checkBoxLabel1.htmlFor = "DOM__checkBox1";
+    checkBoxLabel1.appendChild(document.createTextNode("Include online challenges"));
+    checkDiv3.appendChild(checkBoxLabel1);
+     
+    const checkBox2 = document.createElement("input");
+    checkBox2.type = ("checkbox");
+    checkBox2.id = ("DOM__checkBox2")
+    checkDiv4.appendChild(checkBox2);
+    checkBox2.classList.add("filter__box__checkDiv__checkbox2");
+
+    const checkBoxLabel2 = document.createElement("label");
+    checkBoxLabel2.htmlFor = "DOM__checkBox1";
+    checkBoxLabel2.appendChild(document.createTextNode("Include on-site challenges"));
+    checkDiv4.appendChild(checkBoxLabel2);
+
 }
 
 function closeFilterModal() {
