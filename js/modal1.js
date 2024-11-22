196ad3733082497fc79c8131d7690128e9722469
Ronja Fagerdahl <ronjafagerdahl@gmail.com>
2024-11-22 12:19:37 +0100

    modal part 1 and 2 connected visibly

--- js/modal1.js
+++ js/modal1.js
@@ -6,10 +6,10 @@ console.log(document.querySelector('.custom__date'));
 console.log("hello from modal 1"); //To see if it works
 
 const modal = document.querySelector('.booking-modal');
-
-const step1=document.querySelector('#step1');
-const step2=document.querySelector('#step2');
-const step3=document.querySelector('#step3');
+const steps = document.querySelectorAll('.booking-modal__step')
+const step1 = document.querySelector('#step1');
+const step2 = document.querySelector('#step2');
+const step3 = document.querySelector('#step3');
 
 console.log(modal);
 
@@ -19,11 +19,29 @@ console.log('Hello from the book btn');
 modal.style.display = 'block';
 step2.style.display = 'none';
 step3.style.display = 'none';
-
-
 };
 
+function showSteps(stepNumber){
+    steps.forEach((step, index) => {
+        step.style.display = index + 1 === stepNumber ? 'block' : 'none';
+        step.classList.toggle('booking-modal__step--active', index + 1 === stepNumber);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    modal.style.display = 'none'; //Hidden modal in the start
+    showSteps(1);
+})
+
 const searchButton = document.querySelector('.booking__search-btn');
+
+searchButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    showSteps(2); //Showing step 2 of modal
+})
+
+
+
 const dateInput = document.querySelector('.custom__date');
 
 const displayAvailableTimes = (availableTimes) => {
@@ -49,29 +67,26 @@ const displayAvailableTimes = (availableTimes) => {
 
 searchButton.addEventListener('click', async (event) => {
     event.preventDefault();
-
-    const challengeId=3;
     const selectedDate = dateInput.value;
     
-    if (!selectedDate){
+    if (!selectedDate) {
         alert('Please, select a valid date!');
         dateInput.focus();
         return;
     }
 
     try {
-        const availableTimes = await fetchAvailableTimes(challengeId, selectedDate);
+        const availableTimes = await fetchAvailableTimes(3, selectedDate);
 
         if (availableTimes.success) {
-            console.log('Available times are:', availableTimes.data);
-            displayAvailableTimes(availableTimes);
+            //Hide part 1 and continue to part 2 of modal
+            showSteps(2);
+            console.log('step2');
         } else{
-            console.error('Could not fetch available times:', availableTimes.error);
             alert('Could not fetch. try again later.');
         }
 
     } catch (error) {
-        console.error('Error fetching times:', error);
         alert('Could not fetch available times. Please try again later.');
     }
 });
