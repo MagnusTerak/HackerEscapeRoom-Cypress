1441c6e6cf50a2435b972fef307e173813dce423
El-Duckster <144563447+El-Duckster@users.noreply.github.com>
2024-12-09 15:20:38 +0100

    refactor(challenge): integrate truncateText for challenge class description

--- src/js/utilities/truncateText.js
+++ src/js/utilities/truncateText.js
@@ -1,11 +1,10 @@
 
-export default truncateText = (text, maxLength) => {
+export const truncateText = (text, maxLength) => {
     if (text.length <= maxLength) return text;
   
     const truncated = text.slice(0, maxLength).trimEnd();
     const lastSpaceIndex = truncated.lastIndexOf(' ');
   
-
     return lastSpaceIndex > 0
       ? truncated.slice(0, lastSpaceIndex) + '...'
       : truncated + '...';
